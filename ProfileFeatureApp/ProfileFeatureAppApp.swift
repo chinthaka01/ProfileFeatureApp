b9feca3810bb2dff63512b5b1d6afd0a15d6042d
chinthaka01 <chinthaka01@gmail.com>
2025-12-22 21:45:28 -0500

    Initial Commit.

---
+++ ProfileFeatureApp/ProfileFeatureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileFeatureAppApp.swift
+//  ProfileFeatureApp
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileFeatureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProfileFeatureAppTests/ProfileFeatureAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileFeatureAppTests.swift
+//  ProfileFeatureAppTests
+//
+//  Created by Chinthaka Perera on 12/22/25.
+//
+
+import Testing
+@testable import ProfileFeatureApp
+
+struct ProfileFeatureAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
