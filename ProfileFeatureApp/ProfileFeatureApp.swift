46ae7d97912937349f949f855a99422638fff76e
chinthaka01 <chinthaka01@gmail.com>
2026-01-04 21:55:11 -0500

    Add some DTs.

--- ProfileFeatureApp/ProfileFeatureApp.swift
+++ ProfileFeatureApp/ProfileFeatureApp.swift
@@ -12,12 +12,14 @@ import ProfileFeature
 @main
 struct ProfileFeatureApp: App {
     let analytics = AnalyticsImpl()
-    let profileAPI = ProfileFeatureAPIClient()
+    let networking = NetworkingImpl()
+    let profileAPI: ProfileFeatureAPIClient
     let profileDependencies: ProfileDependenciesImpl
     let profileFactory: ProfileFeatureFactory
     let feature: MicroFeature
     
     init() {
+        profileAPI = ProfileFeatureAPIClient(networking: networking)
         profileDependencies = ProfileDependenciesImpl(profileAPI: profileAPI, analytics: analytics)
         profileFactory = ProfileFeatureFactory(dependencies: profileDependencies)
         feature = profileFactory.makeFeature()
